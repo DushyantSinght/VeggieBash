4a4e4fe0ed253909f4148c99b76f499d509ddf20
Dushyant Singh Tanwar <139846713+DushyantSinght@users.noreply.github.com>
2026-02-18 14:52:42 +0530

    Change multer storage from disk to memory

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,19 +1,5 @@
-// import multer from 'multer';
-// //for uploading images on cloudinary
-// export const upload = multer({storage: multer.diskStorage}); 
 import multer from "multer";
-import path from "path";
 
-// Configure local disk storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // folder where files will be stored
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname)); // e.g. 1691234567890-123.png
-  },
-});
+const storage = multer.memoryStorage();
 
-// Export multer upload middleware
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage });
